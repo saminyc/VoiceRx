ef5cd5a33f0bfbd6c49ee6fb7bc129e3fcf7ce1f
samin <sychowdhury2001@gmail.com>
2026-05-03 22:07:42 -0400

    feat: add conversation database model

---
+++ prisma/migrations/20260504020527_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Conversation" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "question" TEXT NOT NULL,
+    "answer" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Conversation_pkey" PRIMARY KEY ("id")
+);
